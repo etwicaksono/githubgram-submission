d0aedaf52272d9e1df49b58d32c0d8410a0618dc
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-16 05:11:11 +0700

    UPDATE SplashScreen

--- app/src/main/java/com/etwicaksono/submission2/HomeActivity.kt
+++ app/src/main/java/com/etwicaksono/submission2/HomeActivity.kt
@@ -2,10 +2,16 @@ package com.etwicaksono.submission2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        setSupportActionBar(findViewById(R.id.toolbar))
+        return super.onCreateOptionsMenu(menu)
     }
 }
--- app/src/main/java/com/etwicaksono/submission2/SplashScreenActivity.kt
+++ app/src/main/java/com/etwicaksono/submission2/SplashScreenActivity.kt
@@ -1,11 +1,19 @@
 package com.etwicaksono.submission2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity,HomeActivity::class.java))
+            finish()
+        },2000)
     }
 }

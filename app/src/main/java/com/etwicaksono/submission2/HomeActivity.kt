83efc2307c3bc1e27bb8b97655a29bde6332d9d5
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-16 06:04:50 +0700

    UPDATE toolbar, set search init

--- app/src/main/java/com/etwicaksono/submission2/HomeActivity.kt
+++ app/src/main/java/com/etwicaksono/submission2/HomeActivity.kt
@@ -1,17 +1,68 @@
 package com.etwicaksono.submission2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import com.etwicaksono.submission2.databinding.ActivityHomeBinding
+import com.etwicaksono.submission2.databinding.ToolbarMainBinding
 
 class HomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+    private lateinit var homeBinding: ActivityHomeBinding
+
+    companion object {
+        private val TAG = HomeActivity::class.java.simpleName
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        setSupportActionBar(findViewById(R.id.toolbar))
-        return super.onCreateOptionsMenu(menu)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        homeBinding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+
+
+        val toolbar=findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main,menu)
+
+        val searchManager=getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint=resources.getString(R.string.input_username)
+        searchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@HomeActivity,query,Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search -> {
+                Log.d(TAG, "onItemSelected : search")
+            }
+            R.id.about -> {
+                Log.d(TAG, "onItemSelected : about")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/etwicaksono/submission2/UserListFragment.kt
+++ app/src/main/java/com/etwicaksono/submission2/UserListFragment.kt
@@ -1,13 +1,31 @@
 package com.etwicaksono.submission2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.etwicaksono.submission2.databinding.FragmentUserListBinding
 
 
 class UserListFragment : Fragment() {
+
+    private var _binding: FragmentUserListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUserListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 
 }

0fcc45bb79fcd98f9abac3557117bd02bb339d6a
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-17 18:19:10 +0700

    UPDATE UsersListAdapter

--- app/src/main/java/com/etwicaksono/submission2/UsersListAdapter.kt
+++ app/src/main/java/com/etwicaksono/submission2/UsersListAdapter.kt
@@ -1,4 +1,33 @@
 package com.etwicaksono.submission2
 
-class UsersListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.etwicaksono.submission2.databinding.ItemRowUserBinding
+
+class UsersListAdapter(private val listUser: List<ResponseUserItem>) :
+    RecyclerView.Adapter<UsersListAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ItemRowUserBinding.bind(view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_row_user,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.apply {
+            tvName.text=listUser[position].login
+
+            Glide.with(imgUser.context)
+                .load(listUser[position].avatarUrl)
+                .into(imgUser)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listUser.size
+    }
 }

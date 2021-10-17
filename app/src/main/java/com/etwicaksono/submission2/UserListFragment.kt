78fd8a9d8e9bfd35df2ded02f5f53a6b20daae33
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-17 09:44:05 +0700

    UPDATE UserListFragment, set action for onclick about

--- app/src/main/java/com/etwicaksono/submission2/UserListFragment.kt
+++ app/src/main/java/com/etwicaksono/submission2/UserListFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.etwicaksono.submission2.databinding.FragmentUserListBinding
@@ -38,45 +39,58 @@ class UserListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val searchManager= activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        val searchView= binding.toolbar.menu.findItem(R.id.search).actionView as androidx.appcompat.widget.SearchView
+        binding.toolbar.inflateMenu(R.menu.menu_main)
+        val searchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView =
+            binding.toolbar.menu.findItem(R.id.search).actionView as androidx.appcompat.widget.SearchView
 
         searchView.setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
-        searchView.queryHint=resources.getString(R.string.input_username)
-        searchView.setOnQueryTextListener(object:androidx.appcompat.widget.SearchView.OnQueryTextListener{
+        searchView.queryHint = resources.getString(R.string.input_username)
+        searchView.setOnQueryTextListener(object :
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                Toast.makeText(context,"Submited: $query",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Submited: $query", Toast.LENGTH_SHORT).show()
                 return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                Toast.makeText(context,"Changed: $newText",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Changed: $newText", Toast.LENGTH_SHORT).show()
                 return false
             }
 
         })
 
-        binding.toolbar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.search -> {
-                    Log.d(TAG, "onItemSelected : search")
+        binding.toolbar.apply {
+            this.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.search -> {
+                        Log.d(TAG, "onItemSelected : search")
+                    }
+                    R.id.about -> {
+                        val toDetailUserFragment =
+                            UserListFragmentDirections.actionUserListFragmentToDetailUserFragment()
+                        Navigation.findNavController(this).navigate(toDetailUserFragment)
+                        Log.d(TAG, "onItemSelected : about")
+                    }
                 }
-                R.id.about -> {
-
-                    Log.d(TAG, "onItemSelected : about")
-                }
+                true
             }
-            true
         }
 
         val layoutManager = LinearLayoutManager(context)
-        binding.rvUsers.layoutManager = layoutManager
         val itemDecoration = DividerItemDecoration(context, layoutManager.orientation)
-        binding.rvUsers.addItemDecoration(itemDecoration)
 
-        viewModel.listUser.observe(viewLifecycleOwner, { listUser -> setUsersData(listUser) })
+        binding.rvUsers.apply {
+            this.layoutManager = layoutManager
+            this.addItemDecoration(itemDecoration)
+        }
 
-        viewModel.isLoading.observe(viewLifecycleOwner, { isLoading -> showLoading(isLoading) })
+        viewModel.apply {
+            this.listUser.observe(viewLifecycleOwner, { listUser -> setUsersData(listUser) })
+            this.isLoading.observe(viewLifecycleOwner, { isLoading -> showLoading(isLoading) })
+        }
+
+
     }
 
 

1336e7385ef0d75c744386bdcc9764023d2c51e7
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-28 18:59:03 +0700

    ADD Favorite Dao

---
+++ app/src/main/java/com/etwicaksono/githubgram/database/FavoriteDao.kt
@@ -0,0 +1,16 @@
+package com.etwicaksono.githubgram.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface FavoriteDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(mFavorite:Favorite)
+
+    @Delete
+    fun delete(mFavorite:Favorite)
+
+    @Query("SELECT * FROM favorite ORDER BY id ASC")
+    fun getAllFavorites():LiveData<List<Favorite>>
+}

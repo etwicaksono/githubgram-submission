b5b2ad9ac8cba058cde4c0389d5f722174c21c4c
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-28 21:27:35 +0700

    ADD item row user favorite

--- app/src/main/java/com/etwicaksono/githubgram/ui/fragment/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/etwicaksono/githubgram/ui/fragment/favorite/FavoriteViewModel.kt
@@ -1,4 +1,13 @@
 package com.etwicaksono.githubgram.ui.fragment.favorite
 
-class FavoriteViewModel {
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.etwicaksono.githubgram.database.Favorite
+import com.etwicaksono.githubgram.repository.FavoriteRepository
+
+class FavoriteViewModel(application: Application):ViewModel() {
+    private val mFavoriteRepository:FavoriteRepository= FavoriteRepository(application)
+
+    fun getAllNotes():LiveData<List<Favorite>> = mFavoriteRepository.getAllFavorites()
 }

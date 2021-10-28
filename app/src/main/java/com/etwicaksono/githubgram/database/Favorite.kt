c95b89c19c9d443b2337266caa96e430bef8d4b2
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-28 18:55:35 +0700

    Set Favorite

--- app/src/main/java/com/etwicaksono/githubgram/database/Favorite.kt
+++ app/src/main/java/com/etwicaksono/githubgram/database/Favorite.kt
@@ -1,4 +1,21 @@
 package com.etwicaksono.githubgram.database
 
-class Favorite {
-}
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity
+@Parcelize
+class Favorite(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id:Int=0,
+
+    @ColumnInfo(name = "username")
+    var username:String?=null,
+
+    @ColumnInfo(name = "avatar")
+    var avatar:String?=null
+): Parcelable

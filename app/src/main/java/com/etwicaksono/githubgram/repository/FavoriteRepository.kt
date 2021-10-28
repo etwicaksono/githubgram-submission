976d47686716ce20bebebbf1bba5d38bc22ef273
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-28 19:32:22 +0700

    ADD Favorite Repository

---
+++ app/src/main/java/com/etwicaksono/githubgram/repository/FavoriteRepository.kt
@@ -0,0 +1,29 @@
+package com.etwicaksono.githubgram.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.etwicaksono.githubgram.database.Favorite
+import com.etwicaksono.githubgram.database.FavoriteDao
+import com.etwicaksono.githubgram.database.FavoriteRoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class FavoriteRepository(application: Application) {
+    private val mFavoriteDao:FavoriteDao
+    private val executorService:ExecutorService=Executors.newSingleThreadExecutor()
+
+    init {
+        val db=FavoriteRoomDatabase.getDatabase(application)
+        mFavoriteDao=db.favoriteDao()
+    }
+
+    fun getAllFavorites():LiveData<List<Favorite>> =mFavoriteDao.getAllFavorites()
+
+    fun insert(favorite: Favorite){
+        executorService.execute { mFavoriteDao.insert(favorite) }
+    }
+
+    fun delete(favorite: Favorite){
+        executorService.execute { mFavoriteDao.delete(favorite) }
+    }
+}

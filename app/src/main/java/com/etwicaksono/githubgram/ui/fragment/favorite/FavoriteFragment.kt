2bd63f19b8311a3341220b8f5149013df4a00c25
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-28 15:13:29 +0700

    ADD favorite fragment

---
+++ app/src/main/java/com/etwicaksono/githubgram/ui/fragment/favorite/FavoriteFragment.kt
@@ -0,0 +1,7 @@
+package com.etwicaksono.githubgram.ui.fragment.favorite
+
+import androidx.fragment.app.Fragment
+
+class FavoriteFragment : Fragment(){
+
+}
---
+++ app/src/main/java/com/etwicaksono/githubgram/ui/fragment/favorite/FavoriteViewModel.kt
@@ -0,0 +1,4 @@
+package com.etwicaksono.githubgram.ui.fragment.favorite
+
+class FavoriteViewModel {
+}

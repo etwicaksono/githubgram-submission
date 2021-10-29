9d0149fa4b8034aa385529a1d70e1f51400b66ef
Eko Teguh Wicaksono <echotechno007@gmail.com>
2021-10-29 07:06:13 +0700

    Style is working but it's a bad style

--- app/src/main/java/com/etwicaksono/githubgram/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/etwicaksono/githubgram/ui/activity/HomeActivity.kt
@@ -1,15 +1,10 @@
 package com.etwicaksono.githubgram.ui.activity
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import com.etwicaksono.githubgram.databinding.ActivityHomeBinding
 
 
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var homeBinding: ActivityHomeBinding
--- app/src/main/java/com/etwicaksono/githubgram/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/etwicaksono/githubgram/ui/activity/SplashScreenActivity.kt
@@ -1,16 +1,37 @@
 package com.etwicaksono.githubgram.ui.activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.etwicaksono.githubgram.R
+import com.etwicaksono.githubgram.ui.fragment.setting.SettingPreferences
+import com.etwicaksono.githubgram.ui.fragment.setting.SettingViewModel
 
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        val pref = this?.let { SettingPreferences.getInstance(it.dataStore) }
+        val viewModel: SettingViewModel by viewModels { SettingViewModel.Factory(pref) }
+
+        viewModel.getThemeSettings()?.observe(this, { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        })
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@SplashScreenActivity, HomeActivity::class.java))
